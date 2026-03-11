3e0385e8de5099e66320a3937d84d25dafbfc18a
Gabriel-HRsouza <henrique.souzatt54@gmail.com>
2026-03-11 19:39:23 -0400

    feat: update database schema for Aluno

--- ortiz-backend/src/main/java/org/ortiz/fitness/Aluno.java
+++ ortiz-backend/src/main/java/org/ortiz/fitness/Aluno.java
@@ -2,10 +2,12 @@ package org.ortiz.fitness;
 
 import io.quarkus.hibernate.orm.panache.PanacheEntity;
 import jakarta.persistence.Entity;
+import jakarta.persistence.SequenceGenerator;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "aluno")
+@Table(name = "alunos")
+@SequenceGenerator(name = "alunoSeq", sequenceName = "aluno_id_seq", allocationSize = 1, initialValue = 1)
 public class Aluno extends PanacheEntity {
     public String nome;
     public String email;
